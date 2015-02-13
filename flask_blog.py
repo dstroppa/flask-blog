29fd48a9cf31fd0deb0c9c174292efbeca72ce0f
Daniele Stroppa <stroppad@amazon.com>
2015-02-13 06:28:42 +0000

    Changed to Apache license

--- flask_blog.py
+++ flask_blog.py
@@ -6,8 +6,8 @@
     A microblog example application written as Flask tutorial with
     Flask and sqlite3.
 
-    :copyright: (c) 2010 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
+    :copyright: (c) 2015 by Amazon Web Services.
+    :license: Apache 2.0, see LICENSE for more details.
 """
 
 from sqlite3 import dbapi2 as sqlite3
